a44cb94920b13513e6d36ddb469c1efbc1da41bb
Inaboya <momohdavid21@gmail.com>
2021-08-12 17:20:00 +0100

    new files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Landing from "./components/layouts/Landing";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <header>Dev Connector</header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
